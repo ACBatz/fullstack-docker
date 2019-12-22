d3aab96638906bfd8fa1a5ab29626798ba33ec68
Andrew Batzel <andrew.c.batzel@lmco.com>
2019-12-21 20:39:56 -0700

    build scripts

---
+++ bin/build_container.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -d -p 5000:5000 --name batzel-globe ../batzel-globe-image
---
+++ bin/build_images.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build --tag base --file /media/sf_fullstack-docker/Dockerfile.base .
+
+# docker build --tag base --file ../Dockerfile.app .
